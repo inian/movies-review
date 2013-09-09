b21ae51bd7ad605f636724d1d8b20df4d85be4c6
hendychua <hendychua@gmail.com>
2013-09-10 02:04:05 +0800

    various routings and loading of pages done

--- app/assets/javascripts/models/movies.js
+++ app/assets/javascripts/models/movies.js
@@ -23,7 +23,7 @@ $(function() {
     
     var Movies = new MovieList;
     
-    var MovieView = Backbone.View.extend({        
+    var MovieView = Backbone.View.extend({       
         events : {
             'click' : 'clicked'
         },
@@ -36,6 +36,30 @@ $(function() {
         },
         render: function () {
             var template = _.template($('#movie-template').html(), {model: this.model.toJSON()});
+            $(this.el).addClass('span3').html(template);
+            return this;
+        }
+    });
+    
+    var SingleMovieView = Backbone.View.extend({        
+        initialize: function () {
+            _.bindAll(this, 'render', 'remove');
+            //this.model.bind('change', this.render);
+        },
+        render: function () {
+            var template = _.template($('#single-movie-template').html(), {model: this.model});
+            $(this.el).html(template);
+            return this;
+        }
+    });
+    
+    var CreateMovieView = Backbone.View.extend({        
+        initialize: function () {
+            _.bindAll(this, 'render', 'remove');
+            //this.model.bind('change', this.render);
+        },
+        render: function () {
+            var template = _.template($('#create-movie-template').html());
             $(this.el).html(template);
             return this;
         }
@@ -59,20 +83,57 @@ $(function() {
         },
         addAll: function () {
             Movies.each(this.addOne);
+        },
+        viewIndex: function() {
+            $(this.el).empty();
+            this.addAll();
+        },
+        viewSingleMovie: function(movie, reviews) {
+            $(this.el).empty();
+            var model = {movie: movie.toJSON(), reviews: reviews};
+            var view = new SingleMovieView({model: model});
+            $(this.el).append(view.render().el);
+        },
+        createMovie: function() {
+            $(this.el).empty();
+            var view = new CreateMovieView();
+            $(this.el).append(view.render().el);
         }
     });
     
+    // The AppRouter sets up various URLS to the functions.
+    // Inside the respective function, they call the AppViewInstance to
+    // remove the current view and load the correct view.
     var AppRouter = Backbone.Router.extend({
         routes: {
-            "movies/:id" : "view_movie"
+            "" : "index",
+            "movies/:id" : "view_movie",
+            "new_movie" : "new_movie",
+        },
+        index: function() {
+            AppViewInstance.viewIndex();
         },
         view_movie: function(id) {
             var thisMovie = new Movie({id: id+".json"});
             thisMovie.fetch({
                success: function (thisMovie) {
-                   console.log(thisMovie.toJSON());
+                   $.ajax({
+                       type: 'get',
+                       cache: false,
+                       url: 'http://cs3213.herokuapp.com/movies/'+thisMovie.get("id")+"/reviews.json",
+                       dataType: 'json',
+                       error: function(jqXHR, textStatus, error) {
+                           console.log(textStatus + ": " + error);
+                       },
+                       success: function(thisMovieReviews, textStatus, jqXHR) {
+                           AppViewInstance.viewSingleMovie(thisMovie, thisMovieReviews);
+                       }
+                   });
                }
             });
+        },
+        new_movie: function() {
+            AppViewInstance.createMovie();
         }
     });
 
